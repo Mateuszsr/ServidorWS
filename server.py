b1972bd21ea7626545d48c6f5942be0471257f1f
Mateus Ramos Silva <139246429+Mateuszsr@users.noreply.github.com>
2026-03-31 18:11:21 -0300

    Change dashboard HTML file to warspear_dashboard_gxg.html

--- server.py
+++ server.py
@@ -387,7 +387,7 @@ def live():
 @app.route("/", methods=["GET"])
 def dashboard():
     """Serve o dashboard HTML."""
-    html_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "warspear_dashboard.html")
+    html_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "warspear_dashboard_gxg.html")
     if not os.path.exists(html_path):
         return "Dashboard não encontrado.", 404
     return send_file(html_path, mimetype="text/html")
